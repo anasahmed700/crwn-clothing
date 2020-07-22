7b83a383ece33b3a80fadff6b1d9381c8fc2de82
Anas Ahmed <anasahmed700@gmail.com>
2020-07-22 19:58:46 +0500

    stored user data in firestore db & set it to the state of our app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shoppage/ShopPage.jsx'
 import Header from './components/header/Header.jsx'
 import SigninAndSignup from './pages/signin-and-signup/SigninAndSignup';
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 
 class App extends React.Component {
@@ -19,17 +19,30 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
 
-      console.log(user)
+      const userRef = await createUserProfileDocument(userAuth)
+      userRef.onSnapshot(snapshot => {
+        // console.log("snap data: ", snapshot.data())
+        this.setState({currentUser: {
+          id: snapshot.id, 
+          ...snapshot.data()
+        }}, () => console.log(this.state))
+      })
+      
+      } else {
+        this.setState({currentUser: userAuth})
+      }
+
+      // console.log("App.js file: ", user)
     })
   }
 
   componentWillUnmount(){
     this.unsubscribeFromAuth()
   }
-  
+
   render() {
     return (
       <div>
